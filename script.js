5795689eb8cd114f94f6689f85e951f49ca11bb7
Snehal Jadhav <145901611+snehalrj216@users.noreply.github.com>
2026-02-19 13:46:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let openBtn = document.getElementById("openModal");
+let modal = document.getElementById("modal");
+let closeBtn = document.querySelector(".close-modal");
+
+openBtn.onclick = () => modal.style.display = "block";
+closeBtn.onclick = () => modal.style.display = "none";
+
+window.onclick = (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+};
